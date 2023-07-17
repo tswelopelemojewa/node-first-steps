77473e8a0f33187041ae37a5389341b54ad57a87
tswelopelemojewa <tswelopelemojewa@gmail.com>
2023-07-17 15:01:01 +0200

    backend

---
+++ greet1.js
@@ -0,0 +1,5 @@
+export default function greet(name){
+
+    return `Hello, ${name}`;
+}
+
---
+++ greet_me1.js
@@ -0,0 +1,6 @@
+// import the greet module that is in the current folder using CommonJS
+
+import greet from './greet1.js'
+
+// call the function & print the result on the screen
+console.log(greet('tswelopele'))
